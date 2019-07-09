1449e708cd0694c54af7c0b2b145c4b684261c02
Harry <himion0@gmail.com>
2019-07-09 13:14:13 +0100

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-terraform apply -auto-approve
-ip=$(terraform output ip)
-echo $ip
+# terraform apply -auto-approve
+# ip=$(terraform output ip)
+
+ip=18.200.107.240
+
+rc () {
+    ssh ubuntu@$ip $1
+}
+
+rc 'sudo apt update'
