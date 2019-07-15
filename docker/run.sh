a4cc8e6417c95226c766ef0c59547e2f7e7ef216
Harry <himion0@gmail.com>
2019-07-16 00:31:46 +0100

    Removed docker files

--- docker/run.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-docker kill vpn_server
-docker rm vpn_server
-cd docker
-echo ansible | ssh-keygen
-docker build -t vpn_server:latest .
-docker run -t -d -p 2222:22 --name vpn_server vpn_server
